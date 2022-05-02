7a93e9ebde8d62c229741df31c5aeb6ee0615775
JorgeRodriguez1998 <jorgerodriguez5tos@gmail.com>
2022-05-02 14:00:19 -0500

    Updating app.js

--- app.js
+++ app.js
@@ -5,7 +5,17 @@ const app = express()
 app.use(express.json()) // Indicamos que usaremos JSON
 // Puerto en que vamos a ver nuestra app: localhost:3000
 const port = 3000
+
 //routes
+// Methods HTTP: GET, POST, PUT, DELETE
+app.get('/v1/explorers', (req, res) => {
+    console.log(`Get Explorers V1 API ${new Date()}`);
+    const explorer1 = {id: 1, name: "Explorer1"};
+    const explorer2 = {id: 2, name: "Explorer2"};
+    const explorers = [explorer1, explorer2];
+    // HTTP CODE STATUS: 200 (succesful)
+    res.status(200).json(explorers);
+})
 
 // Con esto inicializamos esta app
 app.listen(port, () => {
